6e805b599b9796dc57eedc2215699ff214140911
blockodyssey-kimhojong <kimhojong@blockodyssey.io>
2024-09-01 15:23:56 +0900

    commit

--- src/domains/auth/strategies/jwt.strategy.ts
+++ src/domains/auth/strategies/jwt.strategy.ts
@@ -3,12 +3,15 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AdminsService } from 'src/domains/admins/admins.service';
+import { AuthService } from '../auth.service';
+import { LoginType } from '../enums/login-type.enum';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     private configService: ConfigService,
-    private adminsService: AdminsService
+    private adminsService: AdminsService,
+    private authService: AuthService
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -17,7 +20,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       passReqToCallback: true,
     });
   }
-
+  /*
   async validate(req: any, payload: any) {
     if (payload.isAdmin) {
       const isAllowedIp = await this.adminsService.isAllowedIp(req.ip);
@@ -28,5 +31,27 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     }
     return { userId: payload.sub, walletAddress: payload.walletAddress, isAdmin: payload.isAdmin };
     
+  }
+    */
+  async validate(
+    req: any, 
+    email: string,
+    password: string,
+  ): Promise<any> {
+    const { type, walletAddress } = req.body;
+    console.log("validate");
+    console.log({type, email, password, walletAddress});
+    let user;
+
+    if (type === LoginType.ADMIN) {
+      user = await this.authService.validateAdmin(email, password);
+    } else if (type === LoginType.USER) {
+      user = await this.authService.validateUser(walletAddress);
+    }
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
   }
 }
