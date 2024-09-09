bbb15cdc78c073520f9956d6811d9dab9538a7c3
blockodyssey-kimhojong <kimhojong@blockodyssey.io>
2024-09-10 08:09:42 +0900

    commit

--- src/domains/staking/dto/update-staking-config.dto.ts
+++ src/domains/staking/dto/update-staking-config.dto.ts
@@ -1,9 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, Min } from 'class-validator';
+import { IsNumber, IsOptional, Min } from 'class-validator';
 
 export class UpdateStakingConfigDto {
   @ApiProperty({ example: 5, description: 'The reward amount for staking' })
   @IsNumber()
   @Min(0)
   rewardAmount: number;
+
+  @ApiProperty({ example: 5, description: 'The fee for claim' })
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  claimFee: number;
 }
--- src/domains/staking/entities/staking-config.entity.ts
+++ src/domains/staking/entities/staking-config.entity.ts
@@ -11,11 +11,11 @@ export class StakingConfig {
   @Column('decimal', { precision: 18, scale: 5, name: 'reward_amount' })
   rewardAmount: number;
 
+  @ApiProperty()
+  @Column({ name: 'claim_fee'})
+  claimFee: number;
+
   @ApiProperty({ example: '2023-01-01T00:00:00Z', description: 'Last update time of the configuration' })
   @UpdateDateColumn({ name: 'updated_at' })
   updatedAt: Date;
-
-  @ApiProperty()
-  @Column({ name: 'claim_fee'})
-  claimFee: string;
 }
